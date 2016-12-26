9585ce70090266eb969ac4ce04b1f95aa6c26b81
hhuypm <pmh13797@gmail.com>
2016-12-26 16:00:09 +0700

    insert,update,remove data

---
+++ HapitCore/IDPW.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace HapitCore
+{
+    class IDPW
+    {
+            public ObjectId _id { get; set; }
+            public string ID { get; set; }
+            public string PW { get; set; }
+    }
+}
--- HapitCore/MongoHelper.cs
+++ HapitCore/MongoHelper.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
 
 namespace HapitCore
 {
     public class MongoHelper
     {
+        private MongoDatabase database;
+        public MongoHelper()
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+
+            MongoServer _server = client.GetServer();
+            this.database = _server.GetDatabase("user");
+
+            MongoCollection collection = database.GetCollection<IDPW>("IDPW");
+        }
+        public MongoCollection collection
+        {
+            get
+            {
+                return database.GetCollection<IDPW>("IDPW");
+            }
+        }
+        public void swdata(string ID,string PW)
+        {
+            IDPW sv = new IDPW();
+            sv.ID = ID;
+            sv.PW = PW;
+            collection.Save(sv).ToBsonDocument();
+        }
+        public void updata(string ID,string IDupdate,string PW,string PWupdate)
+        {
+            IMongoQuery query = Query.EQ("ID",ID);
+            if (query != null)
+            {
+                IMongoQuery query1 = Query.EQ("PW", PW);
+                IMongoUpdate update = Update<IDPW>
+                                                 .Set(x => x.ID, IDupdate)
+                                                 .Set(x => x.PW, PWupdate);
+                collection.Update(query1, update);
+            }
+            
+            
+        }
+        public void rmdata(string ID,string PW)
+        {
+            IMongoQuery query = Query.EQ("ID", ID);
+            if (query!=null) {
+                IMongoQuery query1 = Query.EQ("PW", PW);
+                collection.Remove(query1);
+            }
+                
+        }
+
     }
 }
